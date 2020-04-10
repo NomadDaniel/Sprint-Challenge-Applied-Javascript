0128162784df2af29e0c6effe7901a2063cb711c
NomadDaniel <reachingdaniel@gmail.com>
2020-04-10 11:48:36 -0400

    tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -2,8 +2,26 @@
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
 // Once the data is resolved use console logs or breakpoints to review the structure.
-// Iterate over the topics creating a new tab for each topic, and appending it to the DOM
-// under the div.topics element.
-//
+// Iterate over the topics creating a new tab for each topic, and appending it to the DOM under the div.topics element.//
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+function tabCreator(data) {
+  const tab = document.createElement('div')
+  tab.classList.add('tab')
+  tab.textContent = data
+  return tab
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+
+.then(response => {
+  console.log(response)
+  response.data.topics.map(topic => {
+    document.querySelector('.topics').append(tabCreator(topic))
+  })
+})
+
+.catch(error => {
+  console.log('there was a problem returning the tab data' + error)
+})
